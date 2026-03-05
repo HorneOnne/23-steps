16acb61f8d6d7dc5d6dad4c38e64f306323749e4
pal <65548001+HorneOnne@users.noreply.github.com>
2026-03-05 21:20:41 +0700

    Add Economy balance tool and data sets
    Add a new in-browser Economy Balance Tool (wiki/economy.html) to analyze item prices, crafting costs, weapon/armor efficiency and mob loot values. Include dataset files used by the tool: wiki/item_values.js, wiki/crafting_recipes.js, and wiki/mob_configs.js, plus a helper script wiki/rebuild-economy-data.sh to regenerate economy data. Update existing wiki pages (wiki/items.html, wiki/mine-dungeon.html, wiki/mobs.html) to integrate/link the new economy tooling and reflect related content changes.

---
+++ wiki/crafting_recipes.js
@@ -0,0 +1,718 @@
+var CRAFTING_RECIPES = {
+  "Recipes": [
+    {
+      "ResultItem": "Coal",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Dirt",
+          "Quantity": 1
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "CopperOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "IronOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "SilverOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "GoldOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "CopperIngot",
+          "Quantity": 24
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "IronIngot",
+          "Quantity": 24
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "SilverIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "GoldIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "CopperIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "IronIngot",
+          "Quantity": 55
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "SilverIngot",
+          "Quantity": 65
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "GoldIngot",
+          "Quantity": 75
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "CopperIngot",
+          "Quantity": 20
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "CopperIngot",
+          "Quantity": 35
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "CopperIngot",
+          "Quantity": 15
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "IronIngot",
+          "Quantity": 25
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "IronIngot",
+          "Quantity": 40
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "IronIngot",
+          "Quantity": 20
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "SilverIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "SilverIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "SilverIngot",
+          "Quantity": 25
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "GoldIngot",
+          "Quantity": 35
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "GoldIngot",
+          "Quantity": 50
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "GoldIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TinOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "LeadOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "AmberstoneIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Amberstone",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TungstenOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumIngot",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "PlatinumOre",
+          "Quantity": 10
+        },
+        {
+          "ItemID": "Coal",
+          "Quantity": 5
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TinIngot",
+          "Quantity": 25
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TinIngot",
+          "Quantity": 40
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TinIngot",
+          "Quantity": 20
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "LeadIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "LeadIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "LeadIngot",
+          "Quantity": 25
+        }
+      ]
+    },
+    {
+      "ResultItem": "AmberstoneHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "AmberstoneIngot",
+          "Quantity": 35
+        }
+      ]
+    },
+    {
+      "ResultItem": "AmberstoneChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "AmberstoneIngot",
+          "Quantity": 50
+        }
+      ]
+    },
+    {
+      "ResultItem": "AmberstoneLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "AmberstoneIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TungstenIngot",
+          "Quantity": 40
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TungstenIngot",
+          "Quantity": 55
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TungstenIngot",
+          "Quantity": 35
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumHelmet",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "PlatinumIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumChestplate",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "PlatinumIngot",
+          "Quantity": 60
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumLeggings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "PlatinumIngot",
+          "Quantity": 40
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TinIngot",
+          "Quantity": 24
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "LeadIngot",
+          "Quantity": 24
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TungstenIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumPickaxe",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "PlatinumIngot",
+          "Quantity": 30
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TinIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "LeadIngot",
+          "Quantity": 55
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "TungstenIngot",
+          "Quantity": 65
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumSword",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "PlatinumIngot",
+          "Quantity": 75
+        }
+      ]
+    },
+    {
+      "ResultItem": "Strings",
+      "ResultQuantity": 2,
+      "Ingredients": [
+        {
+          "ItemID": "Cobweb",
+          "Quantity": 1
+        }
+      ]
+    },
+    {
+      "ResultItem": "CopperBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 45
+        },
+        {
+          "ItemID": "CopperIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "TinBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 45
+        },
+        {
+          "ItemID": "TinIngot",
+          "Quantity": 45
+        }
+      ]
+    },
+    {
+      "ResultItem": "LeadBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 55
+        },
+        {
+          "ItemID": "LeadIngot",
+          "Quantity": 55
+        }
+      ]
+    },
+    {
+      "ResultItem": "IronBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 55
+        },
+        {
+          "ItemID": "IronIngot",
+          "Quantity": 55
+        }
+      ]
+    },
+    {
+      "ResultItem": "SilverBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 65
+        },
+        {
+          "ItemID": "SilverIngot",
+          "Quantity": 65
+        }
+      ]
+    },
+    {
+      "ResultItem": "TungstenBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 65
+        },
+        {
+          "ItemID": "TungstenIngot",
+          "Quantity": 65
+        }
+      ]
+    },
+    {
+      "ResultItem": "GoldBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 75
+        },
+        {
+          "ItemID": "GoldIngot",
+          "Quantity": 75
+        }
+      ]
+    },
+    {
+      "ResultItem": "PlatinumBow",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "Strings",
+          "Quantity": 75
+        },
+        {
+          "ItemID": "PlatinumIngot",
+          "Quantity": 75
+        }
+      ]
+    },
+    {
+      "ResultItem": "NatureWings",
+      "ResultQuantity": 1,
+      "Ingredients": [
+        {
+          "ItemID": "OakLeaves",
+          "Quantity": 25
+        },
+        {
+          "ItemID": "MangroveLeaves",
+          "Quantity": 25
+        },
+        {
+          "ItemID": "PineLeaves",
+          "Quantity": 25
+        },
+        {
+          "ItemID": "CherryBlossomLeaves",
+          "Quantity": 25
+        },
+        {
+          "ItemID": "BeechLeaves",
+          "Quantity": 25
+        }
+      ]
+    }
+  ]
+};
---
+++ wiki/rebuild-economy-data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# rebuild-economy-data.sh
+# Run this whenever crafting_recipes.json, item_values.json, or mob_configs.json changes.
+# Usage: bash rebuild-economy-data.sh
+
+set -e
+WIKI_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+wrap_json() {
+  local json_file="$1"
+  local js_var="$2"
+  local js_file="$3"
+  printf 'var %s = ' "$js_var" > "$js_file"
+  cat "$json_file" >> "$js_file"
+  printf ';\n' >> "$js_file"
+  echo "  ✓ $js_file"
+}
+
+echo "Rebuilding economy JS data wrappers..."
+wrap_json "$WIKI_DIR/crafting_recipes.json" "CRAFTING_RECIPES" "$WIKI_DIR/crafting_recipes.js"
+wrap_json "$WIKI_DIR/item_values.json"      "ITEM_VALUES"      "$WIKI_DIR/item_values.js"
+wrap_json "$WIKI_DIR/mob_configs.json"      "MOB_CONFIGS"      "$WIKI_DIR/mob_configs.js"
+echo "Done. Open economy.html to see updated data."
